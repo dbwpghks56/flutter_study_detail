be22436de717e3ac61965d385521f1919e0fe93b
JaeHwan <wpghks56@naver.com>
2023-05-20 21:29:11 +0900

    [feature] project init & 색상 상수 지정

---
+++ first/android/app/src/main/kotlin/com/example/first/first/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first.first
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ first/lib/common/const/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// 주 색상
+const PRIMARY_COLOR = Color(0xFF22A45D);
+
+// 글자 색상
+const BODY_TEXT_COLOR = Color(0xFF868686);
+
+// 텍스트 필드 배경 색상
+const INPUT_BG_COLOR = Color(0xFFFBFBFB);
+
+// 텍스트 필드 테두리 색상
+const INPUT_BORDER_COLOR = Color(0xFFF3F2F2);
