f46d4b1cda7e252b5c56edda103d0b45829968fe
JaeHwan <wpghks56@naver.com>
2023-05-20 22:47:11 +0900

    [feature] TextFeild 배치하기

---
+++ first/lib/common/component/custom_text_form_field.dart
@@ -0,0 +1,82 @@
+import 'package:first/common/const/colors.dart';
+import 'package:flutter/material.dart';
+
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool autoFocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField({
+    this.hintText,
+    this.errorText,
+    this.obscureText = false,
+    this.autoFocus = false,
+    required this.onChanged,
+    Key? key
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const baseBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: INPUT_BORDER_COLOR,
+        width: 1.0
+      )
+    );
+
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      // 비밀번호 입력할 때만 사용
+      obscureText: obscureText,
+      autofocus: autoFocus,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.all(20),
+        hintText: hintText,
+        errorText: errorText,
+        hintStyle: const TextStyle(
+          color: BODY_TEXT_COLOR,
+          fontSize: 14.0
+        ),
+        fillColor: INPUT_BG_COLOR,
+        filled: true,
+
+        // 모든 input 상태의 기본 스타일 세팅
+        border: baseBorder,
+
+        // copyWith : 해당 설정을 그대로 가져와 바꾸고 싶은 부분을 쉽게 바꿀 수 있도록 도와준다.
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: PRIMARY_COLOR
+          )
+        )
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- first/lib/main.dart
+++ first/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first/common/component/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,25 @@ class _App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(),
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomTextFormField(
+              hintText: "이메일을 입력해주세요.",
+              onChanged: (value) {
+
+              },
+            ),
+            CustomTextFormField(
+              hintText: "비밀번호를 입력해주세요.",
+              obscureText: true,
+              onChanged: (value) {
+
+              },
+            ),
+          ],
+        )
       ),
     );
   }
