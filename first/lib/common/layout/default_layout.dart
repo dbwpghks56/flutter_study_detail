9f5455fc9dfd582294a1ef1f934b2a0ed2ae56fe
JaeHwan <wpghks56@naver.com>
2023-05-23 00:30:33 +0900

    [feature] 간단한 navbar 및 appbar 제작

--- first/lib/common/layout/default_layout.dart
+++ first/lib/common/layout/default_layout.dart
@@ -5,14 +5,39 @@ class DefaultLayout extends StatelessWidget {
 
   final Color? backgroundColor;
   final Widget child;
+  final String? title;
+  final Widget? bottomNavigationBar;
 
-  const DefaultLayout({required this.child,this. backgroundColor, Key? key}) : super(key: key);
+  const DefaultLayout({required this.child,this.backgroundColor, this.title,
+    this.bottomNavigationBar, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: renderAppBar(),
       backgroundColor: backgroundColor ?? Colors.white,
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    }
+    else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          title!,
+          style: const TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500
+          ),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
   }
 }
--- first/lib/common/view/root_tab.dart
+++ first/lib/common/view/root_tab.dart
@@ -1,15 +1,65 @@
+import 'package:first/common/const/colors.dart';
 import 'package:first/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({Key? key}) : super(key: key);
+
+  @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
+      title: '코팩 딜리버리',
       child: Center(
         child: Text("rootTab"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        // 기본은 shifting, 선택된 것이 크기가 변하고 나머지는 라벨이 안 나온다. 당연히 fixed는 반대
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index) {
+          setState(() {
+            this.index = index;
+          });
+        },
+        currentIndex: index,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: '홈'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.fastfood_outlined),
+              label: '음식'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.receipt_long_outlined),
+              label: '주문'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outlined),
+              label: '프로필'
+          ),
+        ],
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
