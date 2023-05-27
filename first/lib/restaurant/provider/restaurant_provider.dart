6c4f69c55a4b8bd87c65b9fe237b91e9109b7998
JaeHwan <wpghks56@naver.com>
2023-05-28 02:40:43 +0900

    [feature] 너무 졸려서 이까지만..

--- first/lib/restaurant/provider/restaurant_provider.dart
+++ first/lib/restaurant/provider/restaurant_provider.dart
@@ -1,27 +1,28 @@
+import 'package:first/common/model/cursor_pagination_model.dart';
 import 'package:first/restaurant/model/restaurant_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../repository/restaurant_repository.dart';
 
-final restaurantProvider = StateNotifierProvider <RestaurantStateNotifier, List<RestaurantModel>>
+final restaurantProvider = StateNotifierProvider <RestaurantStateNotifier, CursorPagination<RestaurantModel>>
   ((ref) {
     final repository = ref.watch(restaurantRepositoryProvider);
     return RestaurantStateNotifier(repository: repository);
 });
 
-class RestaurantStateNotifier extends StateNotifier<List<RestaurantModel>> {
+class RestaurantStateNotifier extends StateNotifier<CursorPagination<RestaurantModel>> {
   final RestaurantRepository repository;
 
   RestaurantStateNotifier({
     required this.repository
-  }) : super([]){
+  }) : super(){
     paginate();
   }
 
   paginate() async {
     final resp = await repository.paginate();
 
-    state = resp.data;
+    state = resp;
   }
 }
 
