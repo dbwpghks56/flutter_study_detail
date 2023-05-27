914885103d3704cc366b97103a28f3c823dd1888
JaeHwan <wpghks56@naver.com>
2023-05-27 21:08:31 +0900

    [feature] 일단 간단하게 riverpod 테스트 및 설정

--- reverpodTest/lib/main.dart
+++ reverpodTest/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:reverpodtest/screen/home_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: HomeScreen(),
+    ProviderScope(
+      child: MaterialApp(
+        home: HomeScreen(),
+      )
     )
   );
 }
--- reverpodTest/lib/screen/state_provider_screen.dart
+++ reverpodTest/lib/screen/state_provider_screen.dart
@@ -1,18 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:reverpodtest/layout/default_layout.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../riverpod/state_provider_screen.dart';
 
-class StateProviderScreen extends StatelessWidget {
+class StateProviderScreen extends ConsumerWidget {
   const StateProviderScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
     return DefaultLayout(
         title: 'StateProviderScreen',
-        body: Column(
-          children: [
-
-          ],
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                provider.toString(),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    ref.read(numberProvider.notifier).update((state) => state + 1);
+                  },
+                  child: Text(
+                    'Up'
+                  ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (_) => _NextScreen()));
+                },
+                child: Text(
+                  'Go Next'
+                )
+              )
+            ],
+          ),
         )
     );
   }
 }
+
+class _NextScreen extends ConsumerWidget {
+  const _NextScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
+    return DefaultLayout(
+        title: 'StateProviderScreen',
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                provider.toString(),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(numberProvider.notifier).update((state) => state + 1);
+                },
+                child: Text(
+                    'Up'
+                ),
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
