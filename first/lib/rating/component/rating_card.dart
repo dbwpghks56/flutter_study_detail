38b702dff2480fd974873e4ff94789ddd8b19be9
JaeHwan <wpghks56@naver.com>
2023-05-28 19:53:44 +0900

    [feature] review 간단한 UI 작업 내용 표시

--- first/lib/rating/component/rating_card.dart
+++ first/lib/rating/component/rating_card.dart
@@ -26,7 +26,8 @@ class RatingCard extends StatelessWidget {
           rating: rating,
           email: email,
         ),
-        _Body(),
+        const SizedBox(height: 8.0,),
+        _Body(content: content,),
         _Images()
       ],
     );
@@ -74,11 +75,25 @@ class _Header extends StatelessWidget {
 }
 
 class _Body extends StatelessWidget {
-  const _Body({Key? key}) : super(key: key);
+  final String content;
+
+  const _Body({Key? key, required this.content}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Flexible(
+          child: Text(
+            content,
+            style: TextStyle(
+              color: BODY_TEXT_COLOR,
+              fontSize: 14.0
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
 
