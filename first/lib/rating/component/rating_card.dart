a602984fcad0741c4fa84bdd719c7299bade53aa
JaeHwan <wpghks56@naver.com>
2023-05-28 20:07:04 +0900

    [feature] review 간단한 UI 작업 (완)

--- first/lib/rating/component/rating_card.dart
+++ first/lib/rating/component/rating_card.dart
@@ -1,5 +1,6 @@
 import 'package:first/common/const/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
 
 class RatingCard extends StatelessWidget {
   final ImageProvider avatarImage; // network image, asset Image 가 해당
@@ -28,7 +29,12 @@ class RatingCard extends StatelessWidget {
         ),
         const SizedBox(height: 8.0,),
         _Body(content: content,),
-        _Images()
+        if(images.length > 0)
+          SizedBox(
+              height: 100,
+              child: _Images(images: images,)
+          ),
+        const SizedBox(height: 8.0,),
       ],
     );
   }
@@ -98,12 +104,37 @@ class _Body extends StatelessWidget {
 }
 
 class _Images extends StatelessWidget {
-  const _Images({Key? key}) : super(key: key);
+  final List<Image> images;
+
+  const _Images({Key? key, required this.images}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: images.mapIndexed((index, e) => Padding(
+        padding: EdgeInsets.only(right: index == images.length -1 ? 0 : 16.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: e,
+        ),
+      )).toList(),
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
