538d16f30fde2919369a1c0e60889c8dba68dcea
JaeHwan <wpghks56@naver.com>
2023-05-28 15:41:09 +0900

    [feature] restaurant_provider detail hero 위젯으로 애니메이션 자연스럽게 만들기

--- first/lib/restaurant/component/restaurant_card.dart
+++ first/lib/restaurant/component/restaurant_card.dart
@@ -13,6 +13,7 @@ class RestaurantCard extends StatelessWidget {
   final double ratings;
   final bool isDetail;
   final String? detail;
+  final String? heroKey;
 
   const RestaurantCard({
     Key? key,
@@ -25,6 +26,7 @@ class RestaurantCard extends StatelessWidget {
     required this.ratings,
     this.isDetail = false,
     this.detail,
+    this.heroKey
   }) : super(key: key);
 
   factory RestaurantCard.fromModel({
@@ -43,6 +45,7 @@ class RestaurantCard extends StatelessWidget {
         deliveryFee: model.deliveryFee,
         ratings: model.ratings,
         isDetail: isDetail,
+        heroKey: model.id,
       detail: model is RestaurantDetailModel ? model.detail : null,
     );
   }
@@ -51,11 +54,17 @@ class RestaurantCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        if(isDetail)
-          image,
-        if(!isDetail)
+        if(heroKey != null)
+          Hero(
+            tag: ObjectKey(heroKey),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(isDetail ? 0 : 12.0),
+              child: image,
+            ),
+          ),
+        if(heroKey == null)
           ClipRRect(
-            borderRadius: BorderRadius.circular(12.0),
+            borderRadius: BorderRadius.circular(isDetail ? 0 : 12.0),
             child: image,
           ),
         const SizedBox(height: 16.0,),
